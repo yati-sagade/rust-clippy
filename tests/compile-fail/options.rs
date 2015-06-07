314e8c8b2e0a1c82244b5b3cc3a5dc8c2d50dfa9
llogiq <bogusandre@gmail.com>
2015-06-07 15:52:20 +0200

    added more test cases

--- tests/compile-fail/options.rs
+++ tests/compile-fail/options.rs
@@ -1,6 +1,7 @@
 #![feature(plugin)]
 #![plugin(clippy)]
 #![deny(option_and_then_some)]
+#![allow(redundant_closure)]
 
 // the easiest case
 fn and_then_should_be_map(x: Option<i32>) -> Option<i32> {
@@ -20,6 +21,11 @@ fn result_and_then_is_ok(x: Result<i32, ()>) -> Result<i32, ()> {
 // this always returns None
 fn to_none(_: i32) -> Option<i32> { None }
 
+// helper function to add type information to f
+fn check<F>(f: F, o: Option<i32>) where F: FnMut(i32) -> Option<i32> {
+	o.and_then(f);
+}
+
 // need a main anyway, use it get rid of unused warnings too
 fn main() {
 	assert!(and_then_should_be_map(None).is_none());
@@ -28,6 +34,11 @@ fn main() {
 
 	let x : Option<i32> = Some(42);
 	x.and_then(to_none); // nonsense, but no error either
+	// and the same with closure
+	check(|_| None, x); // the same as above with closure
 	
 	x.and_then(|o| if o < 0 { Some(-o) } else { Some(o) }); //~ERROR Consider using _.map(_)
+	x.and_then(|o| Some(o).and_then(|p| Some(p)));  
+	//~^ERROR Consider using _.map(_)
+					//~^^ERROR Consider using _.map(_)
 }
